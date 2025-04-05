93a5812aac8d5dd97a7bafeac7ba41a6710a88a4
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2025-04-05 15:27:04 +0530

    setup

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin: '*',
+    credentials: true
+}))
+
+app.use(express.json({limit: "50mb"}))
+app.use(express.urlencoded({extended: true, limit: "50mb"}))
+app.use(express.static("public"))
+
+// Routes import
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
+
+// Routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
+
+export {app}
--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+import dotenv from 'dotenv'
+import {app} from './app.js'
 
-const app = express();
-const port = 5000;
+dotenv.config();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/lost-items', (req, res) => {
-  // handle database retrieval here
-  res.send([{
-    id: 3, name: 'Phone', description: 'iPhone 12, black case', dateLost: '2023-10-03'
-  }]);
-});
-
-app.post('/lost-items', (req, res) => {
-  console.log(req.body);
-  const { name, description } = req.body;
-  console.log(`Received item: ${name}, ${description}`);
-  res.send('Item added!');
-})
-
-app.get('/scholarships', (req, res) => {
-  // handle database retrieval here
-  res.send([{
-    id: 1, name: 'Scholarship A', description: 'Description of Scholarship A'
-  }, {
-    id: 2, name: 'Scholarship B', description: 'Description of Scholarship B'
-  }]);
-});
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
